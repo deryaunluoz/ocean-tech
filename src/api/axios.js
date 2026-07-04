66fe23b0227cd52bc28e6f13495587fe14645378
deryaunluoz <deryaunluoz@icloud.com>
2026-07-04 15:00:18 +0400

    backend url guncellendi

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://ocean-tech-backend-production.up.railway.app/api'
 })
 
 export default api
